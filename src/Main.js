a8b6eb8343ad2278774245d17de3b5d2a6bd69a0
GigaOwO <takumagiga1118@gmail.com>
2023-07-14 12:14:55 +0900

    new

--- src/Main.js
+++ src/Main.js
@@ -1,28 +1,37 @@
+import { toHaveDisplayValue } from '@testing-library/jest-dom/matchers';
 import React from 'react';
 
 class Main extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            hand: '',
+            playerHand: '',
         };
     }
-    handleHandChange(event) {
-        // var num = Math.floor( Math.random() * 3) + 1;
-        // console.log(num);
-        // if (num === 1) {
-
-        // }
-        // this.setState({
-        //     hand: 
-        // });
+    handleHandChange() {
+        var num = Math.floor( Math.random() * 3);
+        const hands = ['グー', 'チョキ', 'パー'];
+        if (num === 0) {
+            this.setState({
+                playerHand: hands[num],
+            });
+        } else if (num === 1) {
+            this.setState({
+                playerHand: hands[num],
+            });
+        } else if (num === 2) {
+            this.setState({
+                playerHand: hands[num],
+            });
+        }
     }
 
     render() {
-        var num = Math.floor( Math.random() * 3);
-        console.log(num);
         return(
-            <h1>HelloWorld</h1>
+            <div>
+                <h1>{this.state.playerHand}</h1>
+                <button onClick={() => {this.handleHandChange()}}>+</button>
+            </div>
         );
     }
 }
