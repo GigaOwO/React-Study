101d18a6b53a405d77463b9a0383462cbaccbbb6
unknown <takumagiga1118@gmail.com>
2023-07-19 10:20:46 +0900

    new

--- src/Main.js
+++ src/Main.js
@@ -1,4 +1,3 @@
-import { toHaveDisplayValue } from '@testing-library/jest-dom/matchers';
 import React from 'react';
 
 class Main extends React.Component {
@@ -10,62 +9,44 @@ class Main extends React.Component {
             result: '',
         };
     }
+
     handleHandChange() {
         const hands = ['グー', 'チョキ', 'パー'];
-        this.setState({
-            playerHand: hands[Math.floor( Math.random() * 3)]
-        });
-        this.setState({
-            cpuHand: hands[Math.floor( Math.random() * 3)]
-        });
-        if (this.state.playerHand === this.state.cpuHand) {
-            this.setState({
-                result: 'あいこ！'
-            });
-        } else {
-            if (this.state.playerHand === 'グー') {
-                if (this.state.cpuHand === 'パー') {
-                    this.setState({
-                        result: '負け！',
-                    });
-                } else if (this.state.cpuHand === 'チョキ') {
-                    this.setState({
-                        result: '勝ち！',
-                    });
-                }
-            } else if (this.state.playerHand === 'チョキ') {
-                if (this.state.cpuHand === 'グー') {
-                    this.setState({
-                        result: '負け！',
-                    });
-                }
-                if (this.state.cpuHand === 'パー') {
-                    this.setState({
-                        result: '勝ち！',
-                    });
-                }
-            } else if (this.state.playerHand === 'パー') {
-                if (this.state.cpuHand === 'グー') {
-                    this.setState({
-                        result: '勝ち！',
-                    });
-                }
-                if (this.state.cpuHand === 'チョキ') {
-                    this.setState({
-                        result: '負け！',
-                    });
-                }
+        const playerHand = hands[Math.floor(Math.random() * 3)];
+        const cpuHand = hands[Math.floor(Math.random() * 3)];
+
+        this.setState({ cpuHand, playerHand }, () => {
+            // Callback after the state is updated
+            console.log(this.state.playerHand);
+            console.log(this.state.cpuHand);
+
+            if (this.state.playerHand === this.state.cpuHand) {
+                this.setState({
+                    result: 'あいこ！'
+                });
+            } else if (
+                (this.state.playerHand === 'グー' && this.state.cpuHand === 'チョキ') ||
+                (this.state.playerHand === 'チョキ' && this.state.cpuHand === 'パー') ||
+                (this.state.playerHand === 'パー' && this.state.cpuHand === 'グー')
+            ) {
+                this.setState({
+                    result: '勝ち！',
+                });
+            } else {
+                this.setState({
+                    result: '負け！',
+                });
             }
-        }
+        });
     }
 
     render() {
-        return(
+        return (
             <div>
                 <p>player: {this.state.playerHand}</p>
                 <p>cpu: {this.state.cpuHand}</p>
                 <p>{this.state.result}</p>
-                <button onClick={() => {this.handleHandChange()}}>押してください</button>
+                <button onClick={() => { this.handleHandChange() }}>押してください</button>
             </div>
         );
     }
